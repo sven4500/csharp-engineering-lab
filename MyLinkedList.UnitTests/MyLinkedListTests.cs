02a2f2792b0a5e635f1380e0e5d9c63854f16181
sven4500 <sven4500@mail.ru>
2019-11-10 16:10:54 +0300

    Добавлены пакеты NUnit и NUnit.Runners.

--- MyLinkedList.UnitTests/MyLinkedListTests.cs
+++ MyLinkedList.UnitTests/MyLinkedListTests.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework; // TestFixture, Test, Assert
 
 namespace Lab1
 {
+    [TestFixture]
     public class MyLinkedListTests
     {
+
     }
 }
