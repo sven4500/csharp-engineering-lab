f5b2ed642b5cda449f5fa3f8aa18dd461479bc70
sven4500 <sven4500@mail.ru>
2019-12-20 01:43:09 +0300

    Формы Кристины. Поместил в каталог Forms далее распределю каждый по своему каталогу. Проект пока не компилируется.

---
+++ Forms/Manager Staff.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KP
+{
+    /// <summary>
+    /// Логика взаимодействия для Manager_Staff.xaml
+    /// </summary>
+    public partial class Manager_Staff : Page
+    {
+        public Manager_Staff()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Forms/Order list.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KP
+{
+    /// <summary>
+    /// Логика взаимодействия для Order_form.xaml
+    /// </summary>
+    public partial class Order_form : Page
+    {
+        public Order_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
