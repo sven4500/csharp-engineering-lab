8e99cf033f203753bb95286ddfdea54cc610f3df
sven4500 <sven4500@mail.ru>
2019-12-22 14:28:17 +0300

    Программно добавляем вкладки для окна администратора.

--- MainManager/MainManager.xaml.cs
+++ MainManager/MainManager.xaml.cs
@@ -15,9 +15,30 @@ namespace CoffeeShop
 {
     public partial class MainManager : Window
     {
+        // https://stackoverflow.com/questions/15589298/how-to-add-wpf-page-to-tabcontrol
+        void MakeTabs()
+        {
+            Page[] pages = { new ManagerStore(), new ManagerStaff(), new ManagerOrders() };
+
+            for (uint i = 0; i < pages.Length; ++i)
+            {
+                Page page = pages[i];
+
+                Frame frame1 = new Frame();
+                frame1.Content = page;
+
+                TabItem item1 = new TabItem();
+                item1.Content = frame1;
+                item1.Header = page.Title;
+
+                Pages.Items.Add(item1);
+            }
+        }
+
         public MainManager()
         {
             InitializeComponent();
+            MakeTabs();
         }
 
     }
